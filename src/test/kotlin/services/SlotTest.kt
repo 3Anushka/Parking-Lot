788729fb65839053bf545b44a4b14a73eafa74f8
3Anushka <anushkaj@sahaj.ai>
2023-02-17 00:28:35 +0530

    Add Slot class for booking and unbooking a vehicle

---
+++ src/test/kotlin/services/SlotTest.kt
@@ -0,0 +1,36 @@
+package services
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SlotTest{
+    @Test
+    fun `it should check a slot availability`(){
+        val slot = Slot(100)
+        assertFalse(slot.isFull())
+    }
+    @Test
+    fun `it should book a slot for car parking`(){
+        val slot = Slot(100)
+        val firstSlot = slot.bookSlot()
+        val secondSlot = slot.bookSlot()
+
+        assertEquals(1,firstSlot)
+        assertEquals(2,secondSlot)
+
+    }
+
+    @Test
+    fun `it should unparked a car from parking slot`(){
+        val slot = Slot(100)
+
+        val firstSlot = slot.bookSlot()
+        val secondSlot = slot.bookSlot()
+
+        slot.unBookingSlot(1)
+
+        assertFalse(slot.parkedVehicle[firstSlot])
+        assertTrue(slot.parkedVehicle[secondSlot])
+
+    }
+}
